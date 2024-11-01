1bce8e3ea7ef4c85af3c3de43e913a56ad3ff147
disjoint7 <disjoint7@163.com>
2024-11-01 10:03:02 +0800

    将资源文件单独成独立仓库

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from src.utils.logger import setup_logging
 exe_process = None
 
 
-DEBUG = False  # 将其设置为 False 以运行 exe
+DEBUG = True  # 将其设置为 False 以运行 exe
 
 def start_exe_or_script():
     """根据DEBUG状态启动指定的 exe 或 py 文件"""
