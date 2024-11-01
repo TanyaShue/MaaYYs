0a47e27c95cefb0ec15344129ba38099874da710
disjoint7 <disjoint7@163.com>
2024-11-01 09:37:45 +0800

    修改部分文档,优化任务流程

--- src/custom_actions/switch_soul.py
+++ src/custom_actions/switch_soul.py
@@ -25,7 +25,7 @@ class SwitchSoul(CustomAction):
 
         for _ in range(1):
             context.run_pipeline("返回最上页分组", {"返回最上页分组": {"action": "Custom","custom_action": "RandomSwipe","custom_action_param": {"end_roi": [1085, 442, 152, 60],"start_roi": [1085, 161, 162, 58],"delay": 200}}})
-            
+
         print("开始执行自定义动作：点击分组")
         time.sleep(0.5)
 
@@ -47,7 +47,7 @@ class SwitchSoul(CustomAction):
                 context.run_pipeline("下一页",{"下一页": {"action": "Custom","custom_action": "RandomSwipe","custom_action_param": {"start_roi": [1085, 442, 152, 60],"end_roi": [1085, 161, 162, 58],"delay": 400}}})
         else:
             print("点击分组失败")
-        
+
         print("开始执行自定义动作：点击队伍")
         # 点击队伍
         for count in range(1, 10):
@@ -58,7 +58,7 @@ class SwitchSoul(CustomAction):
                 time.sleep(0.5)
                 roi=[detail.box.x-30,detail.box.y-30,500,80]
                 time.sleep(0.5)
-                context.run_pipeline("装备御魂", {"装备御魂": {"action": "Click","timeout":500,"recognition": "TemplateMatch","template": "装备御魂.png","roi": roi,"next": "点击确定"},"点击确定": {"action": "Click","timeout":100,"recognition": "OCR","pre_delay":1000,"expected": "确定","roi": [400,350,475,175]}})
+                context.run_pipeline("装备御魂", {"装备御魂": {"action": "Click","timeout":500,"recognition": "TemplateMatch","post_delay": 1000,"template": "装备御魂.png","roi": roi,"next": "点击确定"},"点击确定": {"action": "Click","timeout":100,"recognition": "OCR","pre_delay":1000,"expected": "确定","roi": [400,350,475,175]}})
                 break
             if count >= 5:
                 context.run_pipeline("返回最上页分队", {"返回最上页分队": {"action": "Custom","custom_action": "RandomSwipe","custom_action_param": {"end_roi": [585, 495, 273, 112],"start_roi": [588, 171, 410, 76],"delay": 400}}})
