8067a8ee1cf0177f96883361f20c474f7cdbae7f
disjoint7 <disjoint7@163.com>
2024-11-05 22:38:08 +0800

    优化悬赏代码

--- src/custom_actions/bounty_monster_recognition.py
+++ src/custom_actions/bounty_monster_recognition.py
@@ -1,49 +1,50 @@
 import time
 import random
-
 from maa.context import Context
 from maa.custom_action import CustomAction
 
 
 class BountyMonsterRecognition(CustomAction):
 
-    def run(self,
-            context: Context,
-            argv: CustomAction.RunArg, ) -> bool:
+    def run(self, context: Context, argv: CustomAction.RunArg) -> bool:
         """
         :param argv:
         :param context: 运行上下文
         :return: 是否执行成功。
         """
         print("开始执行自定义动作：识别悬赏妖怪")
-        cont=0
-        while True:
+        attempts = 0  # 重试计数
+
+        while attempts < 3:
             context.run_pipeline("悬赏封印_识别宝箱")
             img = context.tasker.controller.post_screencap().wait().get()
-            detail = context.run_recognition("悬赏封印_识别妖怪",img)
-            if detail is not None:
+            detail = context.run_recognition("悬赏封印_识别妖怪", img)
+
+            if detail:
                 for result in detail.filterd_results:
-                    # 点击悬赏妖怪
-                    x=random.randint(result.box[0],result.box[0]+result.box[2])
-                    y=random.randint(result.box[1],result.box[1]+result.box[3])
-                    context.tasker.controller.post_click(x,y).wait()
+                    # 随机点击识别到的妖怪位置
+                    x = random.randint(result.box[0], result.box[0] + result.box[2])
+                    y = random.randint(result.box[1], result.box[1] + result.box[3])
+                    context.tasker.controller.post_click(x, y).wait()
                     time.sleep(1)
-                    # 判断悬赏妖怪类型是否为未发现
+
+                    # 检测是否未发现妖怪
                     img = context.tasker.controller.post_screencap().wait().get()
                     detail_not_found = context.run_recognition("识别未发现妖怪", img)
-                    if detail_not_found is None:
+
+                    if detail_not_found:
                         time.sleep(1)
+                        context.run_pipeline("悬赏封印_关闭线索界面")
+                    else:
                         context.run_pipeline("悬赏_开始识别探索")
-                        cont=0
-                        continue
-                    time.sleep(1)
-                    context.run_pipeline("悬赏封印_关闭线索界面")
+                        attempts = 0  # 重置重试计数，继续下一个识别
+                        break
             else:
+                # 未识别到妖怪，滑动继续寻找
                 context.run_pipeline("识别探索目标_向上滑动")
-            print(cont)
-            cont+=1
-            if cont>=3:
-                break
+                attempts += 1  # 增加重试计数
+
+            print(f"尝试次数: {attempts}")
 
         return True
 
