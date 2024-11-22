1b6ad9cba011c91d11ae0c3785d397e875de5bb9
disjoint7 <disjoint7@163.com>
2024-11-22 11:46:45 +0800

    添加资源图片层级,增加图片路径校验脚本

--- src/service/custom_actions/challenge_dungeon_boss.py
+++ src/service/custom_actions/challenge_dungeon_boss.py
@@ -44,11 +44,11 @@ class ChallengeDungeonBoss(CustomAction):
             # 筛选模板容易出现分数不够的情况
 
             context.run_pipeline("点击筛选", {
-                "点击筛选": {"post_delay": 2000, "timeout": 500, "recognition": "TemplateMatch", "template": "地域鬼王_筛选.png", "action": "Click", "target": [1102, 17, 58, 73],"roi":[1077,2,99,121]}})
+                "点击筛选": {"post_delay": 2000, "timeout": 500, "recognition": "TemplateMatch", "template": "地域鬼王/地域鬼王_筛选.png", "action": "Click", "target": [1102, 17, 58, 73],"roi":[1077,2,99,121]}})
             context.run_pipeline("点击热门", {
                 "点击热门": {"post_delay": 2000, "timeout": 500,  "target": [1185, 220, 50, 120], "action": "Click"}})
             context.run_pipeline("识别挑战位置", {
-                "识别挑战位置": {"post_delay": 2000,"timeout": 500, "index": _, "order_by": "Vertical", "recognition": "TemplateMatch", "template": "地鬼_挑战.png", "action": "Click", "roi": [1035, 192, 135, 504]}})
+                "识别挑战位置": {"post_delay": 2000,"timeout": 500, "index": _, "order_by": "Vertical", "recognition": "TemplateMatch", "template": "地域鬼王/地鬼_挑战.png", "action": "Click", "roi": [1035, 192, 135, 504]}})
 
 
             # 选择挑战等级
@@ -68,7 +68,7 @@ class ChallengeDungeonBoss(CustomAction):
             print("等待识别分享按钮")
 
             context.run_pipeline("结束战斗",{"结束战斗": {"next":["点击叉叉"],"interrupt":"点击屏幕继续","timeout":300000,},"点击屏幕继续": {
-                             "roi":[481,641,380,78],"recognition": "OCR", "expected": "点击屏幕继续", "action": "Click", "target": [275,488,652,112]},"点击叉叉": {"recognition": "TemplateMatch", "template": "地鬼_关闭.png", "action": "Click"}})
+                             "roi":[481,641,380,78],"recognition": "OCR", "expected": "点击屏幕继续", "action": "Click", "target": [275,488,652,112]},"点击叉叉": {"recognition": "TemplateMatch", "template": "通用图标/地鬼_关闭.png", "action": "Click"}})
 
         print("自动地鬼挑战完成")
         return True
