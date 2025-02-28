12011b716cf15a74f04ae71b428fa0c20efe97e3
disjoint7 <disjoint7@163.com>
2025-02-28 14:18:53 +0800

    完善添加设备

--- app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# app/models/__init__.py
-from app.models.config.device_config import DeviceConfig
-from app.models.config.resource_config import ResourceConfig
-from app.models.config.global_config import GlobalConfig
---
+++ app/models/config/__init__.py
@@ -0,0 +1,4 @@
+# app/models/config/__init__.py
+from app.models.config.device_config import DeviceConfig
+from app.models.config.resource_config import ResourceConfig
+from app.models.config.global_config import GlobalConfig
