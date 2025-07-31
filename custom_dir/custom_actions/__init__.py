6d7ed1da11f5b810c154d509b35595e88dee1719
disjoint7 <disjoint7@163.com>
2025-07-31 09:52:25 +0800

    chore: 简单的修改

---
+++ custom_dir/custom_actions/__init__.py
@@ -0,0 +1,19 @@
+# custom_dir/custom_actions/__init__.py
+
+__all__ = [
+    "auto_battle",
+    "auto_foster",
+    "bonus_toggle",
+    "bounty_monster_recognition",
+    "challenge_dungeon_boss",
+    "count_action",
+    "human_touch",
+    "loop_action",
+    "question_matcher",
+    "random_swipe",
+    "random_touch",
+    "repeat_challenge_n_times",
+    "switch_soul",
+    "task_list",
+    "team_builder",
+]
