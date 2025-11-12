6db3007cd61a4a01265e34c172abb73c72b2b73e
disjoint7 <disjoint7@163.com>
2025-11-12 17:35:25 +0800

    fix: 修复肝绘卷时缺少的切换预设

--- custom_dir/custom_actions/kun28.py
+++ custom_dir/custom_actions/kun28.py
@@ -8,6 +8,7 @@ from maa.custom_action import CustomAction
 
 from maa.agent.agent_server import AgentServer
 
+
 @AgentServer.custom_action("Kun28")
 class Kun28(CustomAction):
     def __init__(self):
@@ -22,9 +23,11 @@ class Kun28(CustomAction):
         """
 
         print("开始执行自定义动作：Kun28")
-
+        json_data = json.loads(argv.custom_action_param)
+        group_name = json_data.get('group_name')
+        team_name = json_data.get('team_name')
         context.run_task("kun289")
-        self.num_tupo=self.recognition_tupo_number(context)
+        self.num_tupo = self.recognition_tupo_number(context)
 
         context.run_task("返回庭院")
         if self.num_tupo < 20:
@@ -33,8 +36,16 @@ class Kun28(CustomAction):
 
         for _ in range(200):
             if self.num_tupo >= 20:
-                for task in ["返回庭院", "自动结界","返回庭院", "kun2812"]:
-                    context.run_task(task)
+                for task in ["返回庭院", "自动结界", "返回庭院", "kun2812"]:
+                    if task == "自动结界":
+                        context.run_task(task, {"装备突破预设": {
+                            "custom_action_param": {
+                                "group_name": "{value}",
+                                "team_name": "{value}"
+                            }
+                        }})
+                    else:
+                        context.run_task(task)
                     if random.random() < 0.9:
                         number = random.randint(1, 100)  # 90% 概率
                     else:
@@ -50,13 +61,13 @@ class Kun28(CustomAction):
     def recognition_tupo_number(self, context: Context) -> int:
         img = context.tasker.controller.post_screencap().wait().get()
 
-        result=context.run_recognition("kun28_识别突破卷数量",img)
+        result = context.run_recognition("kun28_识别突破卷数量", img)
         if result and result.best_result:
             text = result.best_result.text  # 例如 "18/30"
             self.num_tupo = int(text.split("/")[0])
         print(f"识别到的突破卷数量为:{self.num_tupo}")
 
-        return  self.num_tupo
+        return self.num_tupo
 
     def stop(self):
         pass
